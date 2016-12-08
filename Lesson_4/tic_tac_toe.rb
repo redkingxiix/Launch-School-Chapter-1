f7ef9cf540fc042c243075844d5286526172f3ed
Rory Brookes <rorybrookes89@hotmail.com>
2016-12-08 22:11:43 +0900

    working tic-tac-toe

--- Lesson_4/tic_tac_toe.rb
+++ Lesson_4/tic_tac_toe.rb
@@ -4,8 +4,9 @@ MESSAGES = YAML.load_file('tic_tac_toe.yml')
 
 WIN_CONDITION_ONE = ['a','b','c']
 WIN_CONDITION_TWO = ['1','2','3']
+lang =  ""
 
-def message(message,lang = 'en')
+def message(message,lang)
   MESSAGES[lang][message]
 end
   
@@ -13,9 +14,9 @@ def prompt(text)
   puts "=>#{text}"
 end
 
-def player_choice(board_number, board_data)
+def choice(board_number, board_data,xo)
   if empty_or_valid_choice?(board_number,board_data)
-    board_data[board_number] = "O"
+    board_data[board_number] = xo
   else
     return false
   end
@@ -65,28 +66,90 @@ def print_board(board_data)
   "
 end
 
-play_again = true
-while play_again
+def computer_turn(board_data)
+  computer = ""
+  loop do
+    computer =["a1","a2","a3","b1","b2","b3","c1","c2","c3"].sample
+   break if empty_or_valid_choice?(computer,board_data)
+  end
+  choice(computer,board_data, "X")
+end
 
+input = ''
+loop do
+  prompt(message('language','en'))
+  lang = gets.chomp
+  if lang == "1"
+    lang = "en"
+    break
+  elsif lang == "2"
+    lang = "ja"
+    break
+  end
+end
+
+prompt(message('welcome',lang))
+
+while (input != "1") || (input != "2")
+  input = gets.chomp
+  if input == "1"
+     prompt(message('rules',lang))
+     break
+  elsif input == "2"
+    break
+  else
+    prompt(message('wrong_answer1',lang))
+  end
+end
+
+input = ''
+play_again = true
+
+while play_again
   board_data = {
     "a1" => "", "a2" => "", "a3" => "",
     "b1" => "", "b2" => "", "b3" => "",
     "c1" => "", "c2" => "", "c3" => "",
     }
+
   game_over = false
   while !game_over
     break if !any_free_spaces?(board_data)
-      print_board(board_data)
-      choice = gets.chomp
-      while !player_choice(choice,board_data)
-        prompt("That's not gold kid")
-        choice = gets.chomp
-      end
-      break if winner?(board_data,'x') || winner?(board_data,'O')
+    print_board(board_data)
+    player_choice = gets.chomp
+
+    while !choice(player_choice,board_data,"O")
+      prompt("That's not gold kid")
+      player_choice = gets.chomp
+    end
+      
+    computer_turn(board_data)
+    break if winner?(board_data,'X') || winner?(board_data,'O')
   end
+
   print_board(board_data)
-  input = gets.chomp.to_s
-  if input == 'yes'
-    play_again = false
+
+  if winner?(board_data, "O") 
+    prompt(message('win',lang))
+  elsif winner?(board_data, "X" )
+    prompt(message('lose',lang))
+  else
+    prompt(message('draw',lang))
+  end
+
+
+
+  input =''
+  loop do
+    prompt(message('again',lang))
+    input = gets.chomp.to_s
+    if input == '1'
+      prompt(message('goodbye',lang))
+      play_again = false
+      break
+    elsif input == '2'
+      prompt(message('again',lang))
+      break
+    end
   end
 end
