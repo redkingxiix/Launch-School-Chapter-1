b2edb0e0599513ef309254dfb04d4369d564a88b
Rory Brookes <rorybrookes89@hotmail.com>
2017-02-12 13:36:07 +0900

    create and init board

---
+++ Lesson_4/tic_tac_toe_v2.rb
@@ -0,0 +1,28 @@
+require 'yaml'
+require 'pry'
+MESSAGES = YAML.load_file('tic_tac_toe.yml')
+
+
+def display_board(brd)
+  puts ""
+  puts "     |     |"
+  puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
+  puts "     |     |"
+  puts "_____+_____+_____"
+  puts "     |     |"
+  puts "  #{brd[4]}  |  #{brd[5]}  |  #{brd[6]}"
+  puts "     |     |"
+  puts "_____+_____+_____"
+  puts "     |     |"
+  puts "  #{brd[7]}  |  #{brd[8]}  |  #{brd[9]}"
+  puts "     |     |"
+end
+
+def initialize_board
+  new_board = {}
+  (1..9).each {|num| new_board[num] = ' '}
+  new_board
+end
+
+board = initialize_board
+display_board(board)
