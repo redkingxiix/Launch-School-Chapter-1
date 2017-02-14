4fd003825066c148c96b8a6dda52ccc2a0b584d9
Rory Brookes <rorybrookes89@hotmail.com>
2017-02-14 17:59:51 +0900

    adjust method

--- Lesson_4/twenty_one.rb
+++ Lesson_4/twenty_one.rb
@@ -3,13 +3,15 @@ require 'pry'
 
 def initialize_deck
   suits = ['S','D','H','C']
+  deck = []
   suits.map{|suit|
-      final_suit = []
+    cards= []
     2.upto(10) do |i|
-      final_suit << [suit, i.to_s]
-     end
-    final_suit
+      cards<< [suit, i.to_s]
+    end
+    deck += cards
     }
+    deck
 end
     deck =  initialize_deck
    p deck 
