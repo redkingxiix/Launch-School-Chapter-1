3a309bcdacab0c49d6b93fd26c48532f8da01d62
Rory Brookes <rorybrookes89@hotmail.com>
2017-02-14 17:27:43 +0900

    update method

--- Lesson_4/twenty_one.rb
+++ Lesson_4/twenty_one.rb
@@ -4,12 +4,12 @@ require 'pry'
 def initialize_deck
   suits = ['S','D','H','C']
   suits.map{|suit|
-    final_suit = []
+      final_suit = []
     2.upto(10) do |i|
-      final_suit.concat([suit, i.to_s])
-    end
+      final_suit << [suit, i.to_s]
+     end
     final_suit
-  }
+    }
 end
-deck =  initialize_deck
-    
+    deck =  initialize_deck
+   p deck 
