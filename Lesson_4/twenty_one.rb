ffcb68172b59374ae83b440a7e3d438a9ca4f8bf
Rory Brookes <rorybrookes89@hotmail.com>
2017-02-14 21:30:00 +0900

    use original method, add more methods

--- Lesson_4/twenty_one.rb
+++ Lesson_4/twenty_one.rb
@@ -1,32 +1,13 @@
 require 'pry'
 HIGH_CARDS = ['Jack','Queen','King']
+player_hand = []
+dealer_hand = []
 
-#  def initialize_deck
-#    suits = ['S','D','H','C']
-#    deck = []
-#    suits.map{|suit|
-#      cards= []
-#      1.upto(13) do |i|
-#        i = case i
-#        when 1 then 'Ace'
-#        when 11 then 'Jack'
-#        when 12 then 'Queen'
-#        when 13 then 'King'
-#        else
-#          i
-#        end
-#        cards<< [suit, i.to_s]
-#      end
-#      deck += cards
-#      }
-#      binding.pry
-#      deck
-#  end
-#  Ordered by suit
 def initialize_deck
   suits = ['S','D','H','C']
-  suits.map!{|suit|
-    deck = []
+  deck = []
+  suits.map{|suit|
+    cards= []
     1.upto(13) do |i|
       i = case i
       when 1 then 'Ace'
@@ -36,18 +17,38 @@ def initialize_deck
       else
         i
       end
-      deck<< [suit, i.to_s]
+      cards<< [suit, i.to_s]
     end
-    deck
+    deck += cards
     }
+    deck
 end
+#Ordered by suit
+#def initialize_deck
+#  suits = ['S','D','H','C']
+#  suits.map!{|suit|
+#    deck = []
+#    1.upto(13) do |i|
+#      i = case i
+#      when 1 then 'Ace'
+#      when 11 then 'Jack'
+#      when 12 then 'Queen'
+#      when 13 then 'King'
+#      else
+#        i
+#      end
+#      deck<< [suit, i.to_s]
+#    end
+#    deck
+#    }
+#end
 
 def calculate(hand)
   total_value = 0
   hand.each_with_index do |card, index|
-    card_value = card[index-1][i]
+    card_value = card[1]
     value = 0
-    if FACE_CARDS.any? {|face| face == card_value }
+    if HIGH_CARDS.any? do |face| face == card_value end
       value = 10
     else
       value = card_value.to_i
@@ -59,5 +60,16 @@ def calculate(hand)
   end
 end
 
+def deal_cards(deck)
+  hand = []
+  2.times do 
+    card = deck.sample
+    hand << card
+    deck.index(card)
+    deck.delete(card)
+  end
+  hand
+end
+
 deck =  initialize_deck
-p deck
+player_hand = deal_cards(deck)
